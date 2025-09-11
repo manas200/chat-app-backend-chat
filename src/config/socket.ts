1061300f6310e177150d500dfeaad9d337bd6d89
manas200 <manasc2009@gmail.com>
2025-09-11 05:32:09 +0530

    chat service

---
+++ src/config/socket.ts
@@ -0,0 +1,77 @@
+import { Server, Socket } from "socket.io";
+import http from "http";
+import { Messages } from "../models/Messages.js"; // adjust path to your model
+import express from "express";
+import { Chat } from "../models/Chat.js";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap: Record<string, string> = {};
+
+export const getRecieverSocketId = (recieverId: string): string | undefined => {
+  return userSocketMap[recieverId];
+};
+
+io.on("connection", (socket: Socket) => {
+  console.log("User Connected", socket.id);
+
+  const userId = socket.handshake.query.userId as string | undefined;
+
+  if (userId && userId !== "undefined") {
+    userSocketMap[userId] = socket.id;
+    console.log(`User ${userId} mapped to socket ${socket.id}`);
+  }
+
+  io.emit("getOnlineUser", Object.keys(userSocketMap));
+
+  if (userId) {
+    socket.join(userId);
+  }
+
+  socket.on("typing", (data) => {
+    socket.to(data.chatId).emit("userTyping", {
+      chatId: data.chatId,
+      userId: data.userId,
+    });
+  });
+
+  socket.on("stopTyping", (data) => {
+    socket.to(data.chatId).emit("userStoppedTyping", {
+      chatId: data.chatId,
+      userId: data.userId,
+    });
+  });
+
+  socket.on("joinChat", (chatId) => {
+    socket.join(chatId);
+    console.log(`User ${userId} joined chat room ${chatId}`);
+  });
+
+  socket.on("leaveChat", (chatId) => {
+    socket.leave(chatId);
+    console.log(`User ${userId} left chat room ${chatId}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+    if (userId) {
+      delete userSocketMap[userId];
+      io.emit("getOnlineUser", Object.keys(userSocketMap));
+    }
+  });
+
+  socket.on("connect_error", (error) => {
+    console.log("Socket connection Error", error);
+  });
+});
+
+export { app, server, io };
